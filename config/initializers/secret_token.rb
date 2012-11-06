0908aa7367ddf5be3598524ab8c6bba5ff7daa2c
shinlogin <shinlogin@gmail.com>
2012-11-06 22:03:52 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mail::Application.config.secret_token = '75d1b07645a86e9746690c6a0ae4464c1d1b4c75cafb698e1949ebfebc77b43b6af8e1186a78e6bb8134c2359afceb9e2223300496401029285835a8caf3f2bc'
